fd4c1ee99ba12b667dbb63e1ee696ab7e862421e
LordAnthrax94 <andreagi1994@gmail.com>
2024-11-20 18:41:40 +0100

    fix: completato esercizio 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -2,12 +2,25 @@
 
 const word = 'javascript';
 
+const word2 = 'sternocleidomastoideo';
 
 // Dichiara la funzione qui.
 
+function stringainiziale(stringavocali){ 
+  const vocali = 'aeiou'  
+  let wordvocal = 0;
+  for(let i = 0; i < stringavocali.length; i++){
+    if(vocali.indexOf(stringavocali[i]) !== -1){
+      wordvocal += 1;
+    }
+  }
+  
+  
+  return wordvocal;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(stringainiziale(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
