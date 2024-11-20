f4042d4503a1d5e6f1a8d6b264aadf6d8cd7fbae
LordAnthrax94 <andreagi1994@gmail.com>
2024-11-20 18:05:09 +0100

    fix: completato esercizio 2

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -5,9 +5,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function arrayiniziali(arrayCompleto){ 
+  let newarray = [];   
+  
+  for(i = 0; i < arrayCompleto.length; i++){
+     if(arrayCompleto[i].includes('A')){
+      newarray.push(arrayCompleto[i])
+     }     
+  }
+
+  return newarray;
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(arrayiniziali(names));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
